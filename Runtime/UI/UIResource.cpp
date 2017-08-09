73f0f3874874909fdf97977737133f5996668410
Nick Weihs <nick.weihs@gmail.com>
2017-08-09 01:51:42 -0700

    UI Resource

---
+++ Runtime/UI/UIResource.cpp
@@ -0,0 +1,59 @@
+
+#include "Runtime/RuntimeCommon.h"
+#include "Runtime/UI/UIResource.h"
+#include "Runtime/UI/UIDef.refl.meta.h"
+
+#include "StormData/StormDataJson.h"
+
+#include <sb/vector.h>
+
+UIResource::UIResource(Any && load_data, uint64_t path_hash) :
+  DocumentResourceBase(std::move(load_data), path_hash)
+{
+
+}
+
+NotNullPtr<UIDef> UIResource::GetData()
+{
+  return &m_Data;
+}
+
+DocumentResourceLoadCallbackLink<UIDef, UIResource> UIResource::AddLoadCallback(Delegate<void, NotNullPtr<UIResource>> && callback)
+{
+  if (m_Loaded)
+  {
+    callback(this);
+  }
+
+  return DocumentResourceLoadCallbackLink<UIDef, UIResource>(
+    DocumentResourceReference<UIResource>(this), m_LoadCallbacks.AddDelegate(std::move(callback)));
+}
+
+UIResourcePtr UIResource::Load(czstr file_path)
+{
+  auto resource = LoadDocumentResource(file_path,
+    [](Any && load_data, uint64_t path_hash) -> std::unique_ptr<DocumentResourceBase> { return std::make_unique<UIResource>(std::move(load_data), path_hash); });
+  auto p_this = static_cast<UIResource *>(resource);
+  return UIResourcePtr(DocumentResourceReference<UIResource>(p_this));
+}
+
+DocumentResourceLoadCallbackLink<UIDef, UIResource> UIResource::LoadWithCallback(czstr file_path, Delegate<void, NotNullPtr<UIResource>> && callback)
+{
+  auto resource = LoadDocumentResource(file_path,
+    [](Any && load_data, uint64_t path_hash) -> std::unique_ptr<DocumentResourceBase> { return std::make_unique<UIResource>(std::move(load_data), path_hash); });
+  auto p_this = static_cast<UIResource *>(resource);
+
+  return p_this->AddLoadCallback(std::move(callback));
+}
+
+void UIResource::OnDataLoadComplete(const std::string & resource_data)
+{
+  StormReflParseJson(m_Data, resource_data.data());
+  m_Loaded = true;
+}
+
+void UIResource::CallAssetLoadCallbacks()
+{
+  m_LoadCallbacks.Call(this);
+}
+
--- Runtime/UI/UIResource.h
+++ Runtime/UI/UIResource.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "Foundation/Delegate/DelegateList.h"
+
+#include "Runtime/DocumentResource/DocumentResourceBase.h"
+#include "Runtime/DocumentResource/DocumentResourcePtr.h"
+#include "Runtime/UI/UIDef.refl.h"
+#include "Runtime/Sprite/SpriteResource.h"
+
+class UIResource;
+
+using UIResourcePtr = DocumentResourcePtr<UIDef, UIResource>;
+using UILoadLink = DocumentResourceLoadCallbackLink<UIDef, UIResource>;
+
+class UIResource : public DocumentResourceBase
+{
+public:
+  UIResource(Any && load_data, uint64_t path_hash);
+
+  NotNullPtr<UIDef> GetData();
+  DocumentResourceLoadCallbackLink<UIDef, UIResource> AddLoadCallback(Delegate<void, NotNullPtr<UIResource>> && callback);
+
+  static UIResourcePtr Load(czstr file_path);
+  static DocumentResourceLoadCallbackLink<UIDef, UIResource> LoadWithCallback(czstr file_path, Delegate<void, NotNullPtr<UIResource>> && callback);
+
+protected:
+  virtual void OnDataLoadComplete(const std::string & resource_data) override;
+  virtual void CallAssetLoadCallbacks() override;
+
+private:
+
+  friend class UISystem;
+
+  UIDef m_Data;
+  DelegateList<void, NotNullPtr<UIResource>> m_LoadCallbacks;
+
+  SpritePtr m_Sprite;
+};
