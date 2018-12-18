7b26fd8d79024452a6a3c4328321d7788484982b
hamfirst <nick.weihs@gmail.com>
2018-12-17 23:10:31 -0800

    Base element

--- Test/Assets/UIs/Scripts/Elements/Elem.lua
+++ Test/Assets/UIs/Scripts/Elements/Elem.lua
@@ -1,32 +1,37 @@
 
 Elem = {
-  new = function(elem_meta)
-    elem = {
-      clickable = Clickable:new()
-
-      x = 0
-      y = 0
-      width = 0
-      height = 0
-
-      SetParent = function(self, parent)
-        self.clickable.SetParent(parent.clickable)
-      end
-
-      GetParent = function(self)
-        return self.clickable.GetParent()
-      end
-    }
-
-    elem.clickable.OnUpdate = function(dt)
-      elem.clickable.X = elem.x
-      elem.clickable.Y = elem.y
-      elem.clickable.Width = elem.width
-      elem.clickable.Height = elem.height
-    end
-
-    setmetatable(elem, elem_meta)
-    return elem;
-  end
+  x = 0
+  y = 0
+  width = 0
+  height = 0
 }
+
+function Elem:new(o)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+
+  o.clickable = Clickable:new()
+  o.clickable.OnUpdate(dt) = function (dt)
+    o.Update(dt)
+    o.clickable.X = o.x
+    o.clickable.Y = o.Y
+    o.clickable.Width = o.Width
+    o.clickable.Height = o.Height
+  end
+
+  return o
+end
+
+function Elem:Update(dt)
+
+end
+
+function Elem:SetParent(parent)
+  self.clickable.SetParent(parent.clickable)
+end
+
+function Elem:GetParent(self)
+  return self.clickable.GetParent()
+end
 
