1cce22c456de24fd0b29e35120e7f166a2e91f54
Nick Weihs <nick.weihs@gmail.com>
2018-12-20 02:50:34 -0800

    Menu cleanup fixes

---
+++ Test/Assets/UIs/Scripts/Init/Menu.lua
@@ -0,0 +1,27 @@
+
+cur_menu_elements = {}
+cur_menu_index = 0
+
+function PushMenuElement(elem)
+    cur_menu_elements[cur_menu_index] = elem
+    cur_menu_index = cur_menu_index + 1
+    return elem
+end
+
+function ClearMenuElements()
+    for k, v in pairs(cur_menu_elements) do
+        if v.clickable ~= nil then
+            v.clickable:delete()
+            v.clickable = nil
+        end
+
+        if v.context ~= nil then
+            v.context:delete()
+            v.context = nil
+        end
+    end
+
+    cur_menu_elements = {}
+    cur_menu_index = 0
+end
+
