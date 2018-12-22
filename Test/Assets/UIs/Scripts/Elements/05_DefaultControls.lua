e5df3179fea0226220182f83f583a5589e59c757
Nick Weihs <nick.weihs@gmail.com>
2018-12-22 06:29:07 -0800

    Fixes for default controls

--- Test/Assets/UIs/Scripts/Elements/05_DefaultControls.lua
+++ Test/Assets/UIs/Scripts/Elements/05_DefaultControls.lua
@@ -6,12 +6,15 @@ function DefaultControls:setup(o)
 
   o.audio = AudioControl:new()
   o.audio.x = 0
+  o.audio:SetParent(o)
 
   o.music = MusicControl:new()
   o.music.x = 40
+  o.music:SetParent(o)
 
   o.fullscreen = FullScreenControl:new()
   o.fullscreen.x = 80
+  o.fullscreen:SetParent(o)
 
   o.width = 112
   o.height = 32
