92f44d17bd854af478be40dc801cb5e7b02b524c
hamfirst <nick.weihs@gmail.com>
2019-01-16 00:44:27 -0800

    Map settings is stubbed out

--- Test/Assets/UIs/Scripts/Elements/01_Label.lua
+++ Test/Assets/UIs/Scripts/Elements/01_Label.lua
@@ -7,17 +7,25 @@ Label.b = 0
 Label.alpha = 1.0
 Label.scale = 1.0
 Label.centered = true
+Label.wrap = false
 Label.font = font
 
 function Label:Draw()
 
+  local text = self.text
+  local height = 0
+
+  if self.wrap then
+    text, height = ui:WrapTextScaled(self.font, text, self.width, self.scale)
+  end
+
   if self.centered then
-    ui:DrawCenteredTextScaled(self.font, self.text, self.width / 2, self.height / 2 + 1, 
+    ui:DrawCenteredTextScaled(self.font, text, self.width / 2, self.height / 2 + 1, 
                               self.r, self.g, self.b, self.alpha, kNormal, self.scale);
   else
-    local width, height = ui:MeasureTextScaled(self.font, self.text, self.scale)
+    local width, height = ui:MeasureTextScaled(self.font, text, self.scale)
 
-    ui:DrawTextScaled(self.font, self.text, 0, self.height - height + 1, 
+    ui:DrawTextScaled(self.font, text, 0, self.height - height + 1, 
                               self.r, self.g, self.b, self.alpha, kNormal, self.scale);    
   end
 
