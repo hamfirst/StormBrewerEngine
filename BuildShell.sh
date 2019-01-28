8f420cafefba6036f463424fc38d2b64392536d8
Nick Weihs <nick.weihs@gmail.com>
2019-01-27 21:30:52 -0800

    Reorganizing things for dist server

---
+++ BuildShell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  bash --init-file <(echo ". \"/etc/profile\"; . ./Scripts/SetBuildVars.sh")
+else
+  bash --init-file <(echo ". \"$HOME/.bashrc\"; . ./Scripts/SetBuildVars.sh")
+fi
--- Scripts/BuildShell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  bash --init-file <(echo ". \"/etc/profile\"; . ./SetBuildVars.sh")
-else
-  bash --init-file <(echo ". \"$HOME/.bashrc\"; . ./SetBuildVars.sh")
-fi
