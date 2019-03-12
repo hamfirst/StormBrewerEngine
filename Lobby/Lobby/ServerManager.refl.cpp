dd1a5a2aa409fa19dac7e21083e15bc105acb14e
Nick Weihs <nick.weihs@gmail.com>
2019-03-11 22:52:31 -0700

    Base64 encoding code

---
+++ Lobby/Lobby/ServerManager.refl.cpp
@@ -0,0 +1,31 @@
+
+
+#include <HurricaneDDS/DDSDatabaseConnection.h>
+#include <HurricaneDDS/DDSResponderCall.h>
+
+#include <StormData/StormDataJson.h>
+
+#include "ServerManager.refl.meta.h"
+
+#include <string_view>
+
+#include <stdio.h>
+
+ServerManager::ServerManager(DDSObjectInterface & iface) :
+  m_Interface(iface)
+{
+  FILE * fp = fopen("gcp.json", "rb");
+  if(fp != nullptr)
+  {
+    fseek(fp, SEEK_END, 0);
+    auto len = ftell(fp);
+    fseek(fp, SEEK_SET, 0);
+
+    auto buffer = std::make_unique<char[]>(len);
+    fread(buffer.get(), len, 1, fp);
+
+    StormReflParseJson(m_CredentialsInfo, std::string_view(buffer.get(), len));
+  }
+
+
+}
--- Lobby/Lobby/ServerManager.refl.h
+++ Lobby/Lobby/ServerManager.refl.h
@@ -7,6 +7,43 @@
 
 #include "LobbyConfig.h"
 
+struct GoogleCredentialsInfo
+{
+  STORM_REFL;
+
+  std::string type;
+  std::string project_id;
+  std::string private_key_id;
+  std::string private_key;
+  std::string client_email;
+  std::string client_id;
+  std::string auth_uri;
+  std::string token_uri;
+  std::string auth_provider_x509_cert_url;
+  std::string client_x509_cert_url;
+};
+
+struct GoogleAssertionHeader
+{
+  STORM_REFL;
+
+  std::string alg;
+  std::string kid;
+  std::string typ;
+};
+
+struct GoogleAssertionPayload
+{
+  STORM_REFL;
+
+  std::string iss;
+  std::string scope;
+  std::string aud;
+  std::string iat;
+  std::string exp;
+};
+
+
 struct ServerListGame
 {
   STORM_DATA_DEFAULT_CONSTRUCTION(ServerListGame);
@@ -56,4 +93,5 @@ public:
 
 private:
   STORM_REFL_IGNORE DDSObjectInterface & m_Interface;
+  STORM_REFL_IGNORE GoogleCredentialsInfo m_CredentialsInfo;
 };
