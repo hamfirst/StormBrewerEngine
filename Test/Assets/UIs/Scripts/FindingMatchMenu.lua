ad0256e9c914ac38444b2acf9360a02b3342ca75
hamfirst <nick.weihs@gmail.com>
2019-04-27 04:36:42 -0700

    New UI files

---
+++ Test/Assets/UIs/Scripts/FindingMatchMenu.lua
@@ -0,0 +1,43 @@
+
+function InitFindingMatchMenu()
+
+  local time_delayed = 0
+
+  local label = PushMenuElement(Label:new())
+  label.x = -100
+  label.y = 10
+  label.width = 250
+  label.height = 50
+  label.font = large_header_font
+  label.alpha = 1
+  label.text = "Searching For Match"
+  label.centered = false
+
+  local cancel = PushMenuElement(Button:new())
+  cancel.x = screen_end_x -130
+  cancel.y = screen_start_y + 30
+  cancel.width = 70
+  cancel.height = 20
+  cancel.text = "Back"
+  cancel.back = true
+
+  cancel.Pressed = function()
+    game:Back()
+  end
+
+  function label:Update(dt)
+    time_delayed = time_delayed + dt
+    local periods = math.fmod(time_delayed * 5, 3)
+
+    label.text = "Searching For Match"
+    for i=0, periods + 1 do
+      label.text = label.text .. "."
+    end
+  end
+
+  CreateDefualtControls()
+
+  fader = PushMenuElement(Fader:new())
+  fader:FadeToClear()
+end
+
---
+++ Test/Assets/UIs/Scripts/PlayOnlineMenu.lua
@@ -0,0 +1,24 @@
+
+title_image_texture = loader:LoadTexture("./Images/GameTitle.png")
+
+function InitPlayOnlineMenu()
+
+  local play_menu = PushMenuElement(QuickMenu:new())
+  play_menu:PushMenuAction("Host", function() game:CreatePrivate() end)
+  play_menu:PushMenuAction("Join", function() game:JoinPrivate() end)
+  
+  local start_menu = PushMenuElement(QuickMenu:new())
+  start_menu:PushMenuAction("Competitive", function() game:SearchCompetitive() end)
+  start_menu:PushMenuAction("Casual", function() game:SearchCasual() end)
+  start_menu:PushSubMenu("Private Game", play_menu)
+
+  start_menu:SetBottomButton("Back", function () fader:FadeToSolidThen(function() game:Back() end) end)
+
+  start_menu:Show()
+  
+  CreateDefualtControls()
+  CreatePopupFader()
+
+  fader = PushMenuElement(Fader:new())
+  fader:FadeToClear()
+end
